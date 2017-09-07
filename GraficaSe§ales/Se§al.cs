1e7399c4ccd0bc6129cbfde4ff14828ef6eaeb32
Emiliano Lopez <emiliano@MacBook-Pro-de-Emiliano.local>
2017-09-06 20:22:08 -0700

    Initial Commit

---
+++ GraficaSe§ales/Se§al.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace GraficaSeñales
+{
+    abstract class Señal
+    {
+        public double TiempoInicial { get; set; }
+        public double TiempoFinal { get; set; }
+        public double FrecuenciaMuestreo { get; set; }
+        public PointCollection señal;
+
+        abstract public double evaluar(double t);
+
+        public void construirSeñal()
+        {
+            señal.Clear();
+            double intervaloMuestreo = 1 / FrecuenciaMuestreo;
+            for (double t = TiempoInicial;
+                t <= TiempoFinal;
+                t += intervaloMuestreo)
+            {
+                señal.Add(
+                    new Point(t, evaluar(t)));
+            }
+        }
+    }
+}
